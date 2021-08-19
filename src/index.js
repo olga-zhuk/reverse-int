e91c73833f0063ab648181cb73623e1d2b527fd4
Olga Zhuk <z-o-v2005@yandex.ru>
2021-08-19 22:52:55 +0300

    create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) { n=n*(-1)}
+    let c=n % 10;
+    let b=((n - c) % 100) / 10;
+    let a=(n - 10 * b - c) / 100;
+       if (c === 0) { var numrev = b*10+a }
+         else {numrev = c*100+b*10+a};
+  return numrev;
 }
